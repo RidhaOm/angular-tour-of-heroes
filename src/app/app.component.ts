50baf0956596df4226dcb3e450fd4e9c4391cdf1
RidhaOm <ridhaomrane@gmail.com>
2024-02-08 10:29:09 +0100

    create a feature component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+//import { HeroesComponent } from './heroes/heroes.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  //imports: [HeroesComponent]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
